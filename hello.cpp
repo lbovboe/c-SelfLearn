f6a2166d7c2e75f8f683b23c30ba71e0a54a1b4a
CAIJIANBO <caijianbo@CAIJIANBOs-MacBook-Pro.local>
2020-09-01 16:07:41 +0800

    hello world

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+int main(){
+    string name;
+    cout << "hello world!"<<endl;
+    // cout is the object, arrow is operator help to insert content into object
+    cin >> name;
+    cout << "your name is " << name<<endl;
+    cout << "ENDED"<<endl;
+    cout << "nothing else";
+    return 0;    
+}
